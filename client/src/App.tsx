f4a673f1a6b8f9cdb66db8daef279c53903f8764
Hratch Ghanime <hratchghanime@gmail.com>
2026-03-25 14:01:12 -0700

    feat: add EditModeProvider, EditBar, and admin login route to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/lib/theme-provider";
+import { EditModeProvider } from "@/lib/edit-context";
+import { EditBar } from "@/components/edit-bar";
 import { GradientBackground } from "@/components/gradient-background";
 import AboutPage from "@/pages/about";
 import ExperiencePage from "@/pages/experience";
 import RandomFactsPage from "@/pages/random-facts";
+import AdminLoginPage from "@/pages/admin-login";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -16,6 +19,7 @@ function Router() {
       <Route path="/" component={AboutPage} />
       <Route path="/experience" component={ExperiencePage} />
       <Route path="/random-facts" component={RandomFactsPage} />
+      <Route path="/admin-login" component={AdminLoginPage} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -25,12 +29,15 @@ function App() {
   return (
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <GradientBackground variant="vibrant">
-            <Toaster />
-            <Router />
-          </GradientBackground>
-        </TooltipProvider>
+        <EditModeProvider>
+          <TooltipProvider>
+            <GradientBackground variant="vibrant">
+              <Toaster />
+              <Router />
+              <EditBar />
+            </GradientBackground>
+          </TooltipProvider>
+        </EditModeProvider>
       </QueryClientProvider>
     </ThemeProvider>
   );
